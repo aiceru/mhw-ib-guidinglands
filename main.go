838913ffc3ae238c980b37d94048a569d1ae4cb7
Wooseok Son <aiceru@gmail.com>
2019-10-02 18:45:45 +0900

    Make monster list

--- main.go
+++ main.go
@@ -34,6 +34,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -41,7 +43,6 @@ import (
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
 )
 
@@ -50,7 +51,7 @@ func getClient(config *oauth2.Config) *http.Client {
 	// The file token.json stores the user's access and refresh tokens, and is
 	// created automatically when the authorization flow completes for the first
 	// time.
-	tokFile := "token.json"
+	tokFile := "data/token.json"
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
 		tok = getTokenFromWeb(config)
@@ -100,7 +101,7 @@ func saveToken(path string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
-func main() {
+func getData(monsters []Monster) error {
 	b, err := ioutil.ReadFile("data/credentials.json")
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
@@ -113,27 +114,96 @@ func main() {
 	}
 	client := getClient(config)
 
-	srv, err := sheets.New(client)
+	srv, err := sheets.NewService(context.Background(),
+		option.WithScopes(sheets.SpreadsheetsReadonlyScope),
+		option.WithHTTPClient(client))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
 
 	// Prints the names and majors of students in a sample spreadsheet:
 	// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-	spreadsheetId := "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
-	readRange := "Class Data!A2:E"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	spreadsheetId := "1YTO5fcXNmfpU7XptQRuxmZIkvWq5bly-L8j3qq22oy8"
+	readRange := "인도하는 땅 몬스터 소재!B12:G53"
+	mlist, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+		return err
 	}
 
-	if len(resp.Values) == 0 {
+	makeMonsterList(monsters, mlist)
+
+/*	readRange = "출현 몬스터 Ver.가로!B18:I38"
+	forest, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+		return err
+	}
+
+	readRange = "출현 몬스터 Ver.가로!B43:I62"
+	wildspire, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+		return err
+	}
+
+	readRange = "출현 몬스터 Ver.가로!B67:I83"
+	coral, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+		return err
+	}
+
+	readRange = "출현 몬스터 Ver.가로!B88:I100"
+	rotten, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+		return err
+	}*/
+
+	/*if len(resp.Values) == 0 {
 		fmt.Println("No data found.")
 	} else {
 		fmt.Println("Name, Major:")
 		for _, row := range resp.Values {
 			// Print columns A and E, which correspond to indices 0 and 4.
-			fmt.Printf("%s, %s\n", row[0], row[4])
+			fmt.Printf("%s, %s, %s, %s\n", row[0], row[1], row[2], row[3])
 		}
+	}*/
+	return nil
+}
+
+func makeMonsterList(monsters []Monster, v *sheets.ValueRange) {
+	if len(v.Values) == 0 {
+		log.Println("No data found.")
+		return
 	}
+	for i, row := range v.Values {
+		monsters[i].Name = row[0].(string)
+	}
+}
+
+const (
+	Normal		= iota
+	Tempered
+)
+
+type Monster struct {
+	Name 		string
+	Info		map[string]MonsterInfo
+}
+
+type MonsterInfo struct {
+	Type		int
+	Item 		string
+	Habitat		[4][7]int
+}
+
+func marshalSheet(valueRange *sheets.ValueRange) {
+
+}
+
+func main() {
+	monsters := make([]Monster, 42)
+	getData(monsters)
 }
